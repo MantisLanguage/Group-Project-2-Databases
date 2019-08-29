3b4ae47e7e4affa13a49fc091f519e4a3edad89d
Alex Powarzynski <alexpowarzynski@me.com>
2019-08-28 20:38:44 -0700

    Added base sequelize features and server settings

---
+++ models/post.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes) {
+  let Post = sequelize.define("Post", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  Post.associate = function(models) {
+    Post.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Post;
+};
